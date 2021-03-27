95f68c40fcb49f506d53d185bec35f22ae5d77cc
Allen Garvin <aurvondel@gmail.com>
2021-03-27 13:03:58 -0500

    oops

--- 2015/13/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#define MOD(A, B) (A % B < 0) ? B + (A % B) : (A % B)
-
-int main() {
-    printf("%d\n", MOD(-1, 8));
-    printf("%d\n", MOD(8, 8));
-    printf("%d\n", MOD(9, 8));
-}
