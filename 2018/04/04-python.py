e5ff29d64c9f44bb7246f020a2012d8f6f2066fb
Allen Garvin <aurvondel@gmail.com>
2021-03-31 15:04:29 -0500

    Rewritten, python3

--- 2018/04/04-python.py
+++ 2018/04/04-python.py
@@ -1,17 +1,45 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import sys, os, argparse, operator, re, itertools
-from datetime import datetime
-from parse import parse
+import datetime as dt
 
-def main(args):
-    for line in open(args.file):
-        p = parse("[{tm}] {rt}\n", line)
-        dt = datetime.strptime(p["tm"], "%Y-%m-%d %H:%M")
-        print dt
+def read_log(fn):
+    guard_times = dict()
 
-if __name__ == "__main__":
-    day = sys.argv[0].split("-")[0]
-    ap = argparse.ArgumentParser(description="2018 Day {0} AOC: FOO FOO".format(day))
-    ap.add_argument("file", help="Input file", default="{0}-input.txt".format(day), nargs="?")
-    main(ap.parse_args())
+    lines = sorted([x.replace("1518", "2018") for x in open(fn)])
+
+    for l in lines:
+        tm = dt.datetime.strptime(l[:18].replace("1518", "2018"), "[%Y-%m-%d %H:%M]")
+        l = l[19:].strip()
+        if l.startswith("Guard #"):
+            guard_num = int(l[7:].split()[0])
+            arr = guard_times.get(guard_num, [])
+            guard_times[guard_num] = arr
+        else:
+            arr += [tm]
+    return guard_times
+
+def intervals(time_arr):
+    return [(a.minute, b.minute) for a, b in zip(time_arr[::2], time_arr[1::2])]
+
+def total_minutes(time_arr):
+    return sum([(b-a) for a, b in intervals(time_arr)])
+
+def sleepiest_minute(ints):
+    minutes = dict()
+    for a, b in intervals(ints):
+        for i in range(a, b):
+            minutes[i] = minutes.get(i, 0) + 1
+
+    return sorted(minutes.keys(), key=lambda x: minutes[x])[-1]
+
+def main(fn):
+    times = read_log(fn)
+
+    totals = { g : total_minutes(tms) for g, tms in times.items() }
+    sleepiest = sorted(totals.keys(), key=lambda g: totals[g])[-1]
+
+    print(sleepiest * sleepiest_minute(times[sleepiest]))
+    
+    guards_by_minute = { g : sleepiest_minute(tms) for g, tms in times.items() if tms }
+    minutest_guard = sorted(guards_by_minute.keys(), key=lambda g: guards_by_minute[g])[-1]
+    print(minutest_guard * guards_by_minute[minutest_guard])
