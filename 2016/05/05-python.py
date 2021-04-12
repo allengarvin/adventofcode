d0af94a181957b95480f988e28daf5025d3b7d46
Allen Garvin <aurvondel@gmail.com>
2021-04-12 13:58:20 -0500

    Python

--- 2016/05/05-python.py
+++ 2016/05/05-python.py
@@ -1,9 +1,26 @@
 #!/usr/local/bin/python3.7
 
-import sys, argparse, operator, re
+import sys, argparse
+from hashlib import md5
 
 def main(args):
-    open(args.file)
+    seed = open(args.file).read().strip()
+
+    pos, i = 0, 0
+    passwd1, passwd2 = "", [None] * 8
+
+    while pos < 8 or None in passwd2:
+        hash = md5((seed + str(i)).encode("utf-8")).hexdigest()
+        if hash.startswith("00000"):
+            passwd1 += hash[5]
+            ndx = int(hash[5], 16)
+            if ndx < 8 and not passwd2[ndx]:
+                passwd2[ndx] = hash[6]
+                print(hash, passwd2)
+            pos += 1
+        i += 1
+    print(passwd1[:8])
+    print("".join(passwd2))
     
 
 if __name__ == "__main__":
