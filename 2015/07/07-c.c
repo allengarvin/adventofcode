7307f8bd667d9df94b12e80d687852e7f6ec3749
Allen Garvin <aurvondel@gmail.com>
2021-04-16 15:21:33 -0500

    c

--- 2015/07/07-c.c
+++ 2015/07/07-c.c
@@ -8,10 +8,12 @@
 
 #define TYPE_INT 1
 #define TYPE_VAR 2
+unsigned short variables[676];
+
 struct instruction {
     unsigned short a, b, target;
 
-    int a_type;
+    int a_type, b_type;
     unsigned short (*func)(unsigned short, unsigned short);
 };
 
@@ -19,10 +21,21 @@ unsigned short NOT(unsigned short x, unsigned short _) {
     return 65535 - x;
 }
 
-unsigned short ASSIGN(unsigned short a, unsigned short __) {
+unsigned short ASSIGN(unsigned short a, unsigned short _) {
     return a;
 }
 
+unsigned short LSHIFT(unsigned short a, unsigned short b) {
+}
+
+unsigned short RSHIFT(unsigned short a, unsigned short b) {
+}
+
+unsigned short AND(unsigned short a, unsigned short b) {
+}
+
+unsigned short OR(unsigned short a, unsigned short b) {
+}
 
 struct instruction *parse(char *left, char *right) {
     struct instruction *instr;
@@ -33,6 +46,11 @@ struct instruction *parse(char *left, char *right) {
     a = strtok(left, " ");
     b = strtok(NULL, " ");
     c = strtok(NULL, " ");
+
+    if( strlen(right) == 1 )
+        instr->target = right[0] - 97;
+    else 
+        instr->target = (right[0] - 97) * 26 + (right[1] - 97);
 
     if( !b && !c ) {
         if( isdigit(a[0]) ) {
@@ -47,11 +65,9 @@ struct instruction *parse(char *left, char *right) {
         }
         instr->func = &ASSIGN;
         return instr;
-    }
-    if( isdigit(a[0]) ) {
-        instr->a_type = TYPE_INT;
-        instr->a = (unsigned short) atoi(a);
-    } else if( strcmp(a, "NOT") == 0 ) {
+    }       // ASSIGN
+
+    if( strcmp(a, "NOT") == 0 ) {
          instr->a_type = TYPE_VAR;
          if( strlen(b) == 2 )
             instr->a = (b[0] - 97) * 26 + (b[1] - 97);
@@ -59,6 +75,13 @@ struct instruction *parse(char *left, char *right) {
             instr->a = b[0] - 97;
         instr->func = &NOT;
         return instr;
+    }       // NOT
+
+    // HERE on out we just have arg OPERATION arg
+
+    if( isdigit(a[0]) ) {
+        instr->a_type = TYPE_INT;
+        instr->a = (unsigned short) atoi(a);
     } else {
         instr->a_type = TYPE_VAR;
         if( strlen(a) == 2 )
@@ -66,8 +89,45 @@ struct instruction *parse(char *left, char *right) {
         else
             instr->a = a[0] - 97;
     }
+    if( isdigit(c[0]) ) {
+        instr->b_type = TYPE_INT;
+        instr->b = (unsigned short) atoi(c);
+    } else {
+        instr->b_type = TYPE_VAR;
+        if( strlen(c) == 2 )
+            instr->b = (c[0] - 97) * 26 + (c[1] - 97);
+        else
+            instr->b = c[0] - 97;
+    }
+    if( strcmp(b, "LSHIFT") == 0 ) {
+        instr->func = &LSHIFT;
+        return instr;
+    }
+    if( strcmp(b, "RSHIFT") == 0 ) {
+        instr->func = &RSHIFT;
+        return instr;
+    }
+    if( strcmp(b, "AND") == 0 ) {
+        instr->func = &AND;
+        return instr;
+    }
+    if( strcmp(b, "OR") == 0 ) {
+        instr->func = &OR;
+        return instr;
+    }
+
     printf("a=<%s> b=<%s> c=<%s>\n", a, b, c);
     return instr;
+}
+
+char *print_variable(int n) {
+    char *v;
+
+    v = (char *) malloc(3);
+    v[2] = 0;
+    v[0] = n > 25 ? n / 26 + 97 : 32;
+    v[1] = n % 26 + 97;
+    return v;
 }
 
 void execute(struct instruction *t) {
@@ -75,7 +135,11 @@ void execute(struct instruction *t) {
         printf("NOT\n");
     }
     if( t->func == ASSIGN ) {
-        printf("ASSIGN\n");
+        if( t->a_type == TYPE_INT )
+            printf("ASSIGN: %s := %d\n", print_variable(t->target), t->a);
+        else
+            printf("ASSIGN: %s := %s\n", print_variable(t->target), print_variable(t->a));
+                
     }
 }
 
@@ -84,6 +148,9 @@ int main(int argc, char *argv[]) {
     char buf[80];
     struct instruction instructs[500], *t;
     int i_ptr = 0;
+
+    for( int i=0; i<676; i++ )
+        variables[i] = 0;
 
     if( !(fd = fopen(FN, "r")) ) {
         fprintf(stderr, "%s: %s\n", FN, strerror(errno));
