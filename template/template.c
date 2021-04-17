75c85afa56fe7dd6a40d90d3a43398dc1cea9d5e
Allen Garvin <aurvondel@gmail.com>
2021-04-17 15:51:31 -0500

    Template so I don't have to retype so much

---
+++ template/template.c
@@ -0,0 +1,47 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    FILE *fd;
+    char *fn;
+
+    if( argc > 1 )
+        fn = argv[1];
+    else {
+        fn = (char *) malloc(13);
+        strncat(fn+2, "-input.txt", 12);
+
+        if( isdigit(argv[0][0]) && isdigit(argv[0][1]) && argv[0][2] == '-' ) {
+            fn[0] = argv[0][0];
+            fn[1] = argv[0][2];
+        } else {
+            char *buf;
+            int i;
+
+            buf = (char *) malloc(80);
+            getcwd(buf, 79);
+            
+            for( i=strlen(buf); i; i-- )
+                if( buf[i] == '/' )
+                    break;
+
+            if( isdigit(buf[i+1]) && isdigit(buf[i+2]) ) {
+                fn[0] = buf[i+1];
+                fn[1] = buf[i+2];
+            } else {
+                fprintf(stderr, "%s: unable to determine input filename from executable or PWD\n", argv[0]);
+                return EXIT_FAILURE;
+            }
+            free(buf);
+        }
+    }
+    fd = fopen(fn, "r");
+    if( !fd ) {
+        fprintf(stderr, "%s: %s\n", fn, strerror(errno));
+        return 1;
+    }
+}
