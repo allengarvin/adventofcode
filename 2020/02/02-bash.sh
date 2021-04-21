d53ef13646bd95e2209f0a7a4715e694a5a5bcc2
Allen Garvin <aurvondel@gmail.com>
2021-04-21 10:37:16 -0500

    rewritten Bash

--- 2020/02/02-bash.sh
+++ 2020/02/02-bash.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-total1=0
-total2=0
-
-cat ${1:-02-input.txt} | tr ':-' \ | while read a b c d; do 
-    count=$(echo $d | tr -dc $c | wc -c)
-    ((total1 += ($count >= $a && count <= $b) ))
+PART2=$(mktemp /tmp/aoc2020-02-2.XXXXXXX)
+sed 's/[-:]/ /g' 02-input.txt | while read a b c d; do
+    echo $d | tr -dC $c | egrep "$c{$a,$b}" | egrep -v "$c{$(($b+1))}"
     ((a--)); ((b--))
-    if [[ (${d:$a:1} == $c && ${d:$b:1} != $c) || (${d:$a:1} != $c && ${d:$b:1} == $c) ]]; then
-        ((total2 += 1))
-    fi
-    echo $total1
-    echo $total2
-done | tail -2      # stupid bash scoping
+    echo ${d:$a:1}${d:$b:1} | tr -dC $c | grep '^.$' >> $PART2
+done | wc -l
 
+wc -l <$PART2
+rm -f $PART2
 
