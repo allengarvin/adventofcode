e3f9e17dad6ef6e0407ce1e4f72d873bcb3f6576
Allen Garvin <aurvondel@gmail.com>
2021-04-21 10:39:20 -0500

    deleted line

--- 2020/05/05-bash.sh
+++ 2020/05/05-bash.sh
@@ -2,7 +2,6 @@
 
 TMP=$(mktemp /tmp/aoc2020-05-XXXXXX)
 tr FBRL 0110 < 05-input.txt | while read a; do echo $(( 2#$a )); done | sort -n > $TMP
-cat $TMP > /tmp/aoc
 tail -1 $TMP
 for i in $(seq $(head -n 1 $TMP) $(tail -1 $TMP)); do grep -qL ^$i$ $TMP || break; done
 echo $i
