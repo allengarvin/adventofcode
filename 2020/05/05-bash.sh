177ad9bc04e1bc8e5cd44091df8008d322c607b2
Allen Garvin <aurvondel@gmail.com>
2021-04-21 16:08:17 -0500

    refactor

--- 2020/05/05-bash.sh
+++ 2020/05/05-bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TMP=$(mktemp /tmp/aoc2020-05-XXXXXX)
-(echo ibase=2; tr FBRL 0110 < 05-input.txt) | bc | sort -n > $TMP
+(echo ibase=2; tr FBRL 0110 < 05-input.txt | sort ) | bc > $TMP
 tail -1 $TMP
 comm -13 $TMP <(seq $(sed -n '1p;$p' $TMP))
 rm $TMP
