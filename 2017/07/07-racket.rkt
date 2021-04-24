19d026b32b75bc0a8f008cf1d699ec03d998502b
Allen Garvin <aurvondel@gmail.com>
2021-04-23 19:53:05 -0500

    Racket

--- 2017/07/07-racket.rkt
+++ 2017/07/07-racket.rkt
@@ -19,5 +19,26 @@
                  (null? (filter (λ (j) (member i (last (hash-ref *prog-hash* j)))) *prog-names*))) *prog-names*)))
 
 (displayln *root*)
-; part 1 finished
+
+(define *prog-weights* (make-hash))
+
+(define (generate-weights root progs)
+  (let* ([wt (car (hash-ref progs root))]
+         [subwt (apply + (for/list ([i (second (hash-ref progs root))])
+                                 (generate-weights i progs)))])
+    (hash-set! *prog-weights* root (+ wt subwt))
+    (+ wt subwt)))
+
+(define root-weight (generate-weights *root* *prog-hash*)) ; don't need this for anything
+
+(define (drill-down root wt)
+  (let ([groups (group-by (λ (s) (hash-ref *prog-weights* s)) (last (hash-ref *prog-hash* root)))])
+    (if (= 2 (length groups))
+        (let ([diff (apply - (map (λ (g) (hash-ref *prog-weights* (car g))) (sort groups (λ (a b) (> (length a) (length b))))))])
+          (drill-down (caar (filter (λ (g) (= 1 (length g))) groups)) diff))
+        (+ (car (hash-ref *prog-hash* root)) wt))))
+
+(displayln (drill-down *root* 0))
+
+
   
