1817600e170b25ad03df091ea008c5a324206eba
Allen Garvin <aurvondel@gmail.com>
2021-12-01 09:03:26 -0600

    Python

---
+++ 2021/01/01-python.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys, os, argparse, operator, re
+
+def increasingp(lst):
+    return len([n for n, x in enumerate(lst[1:]) if x > lst[n]])
+
+def main(args):
+    nums = [int(x) for x in open(args.file)]
+    print("Problem 1: {}".format(increasingp(nums)))
+    print("Problem 2: {}".format(increasingp([sum(nums[i:i+3]) for i in range(len(nums)-2)])))
+
+if __name__ == "__main__":
+    default_file = sys.argv[0].split("-")[0] + "-input.txt"
+    ap = argparse.ArgumentParser(description="2021 Day 1 AOC: Sonar Sweep")
+    ap.add_argument("file", help="Input file", default=default_file, nargs="?")
+    main(ap.parse_args())
+    
