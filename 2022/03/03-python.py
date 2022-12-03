15f4c849826f53f3e3c0b14dfeb5bbb74dc2c51c
Allen Garvin <aurvondel@gmail.com>
2022-12-03 10:35:27 -0600

    minor refactor

--- 2022/03/03-python.py
+++ 2022/03/03-python.py
@@ -5,21 +5,20 @@ import sys, argparse, operator, re, string
 def score(c):
     return ord(c)-96 if c in string.ascii_lowercase else ord(c)-64+26
 
-def main(args):
-    part1, part2 = 0, 0
+def intersect(set_list):
+    sc = 0
+    for s in set_list:
+        for c in set.intersection(*[set(x) for x in s]):
+            sc += score(c)
+    return sc
 
+def main(args):
     lines = [x.strip() for x in open(args.file)]
     elf_groups = [lines[i:i+3] for i in range(0, len(lines), 3)]
     sacks = [[x[:len(x)//2], x.strip()[len(x)//2:]] for x in lines]
 
-    for s in sacks:
-        for c in set.intersection(*[set(x) for x in s]):
-            part1 += score(c)
-    for e in elf_groups:
-        for c in set.intersection(*[set(x) for x in e]):
-            part2 += score(c)
-    print(part1)
-    print(part2)
+    print(intersect(sacks))
+    print(intersect(elf_groups))
 
 if __name__ == "__main__":
     day = sys.argv[0].split("-")[0]
